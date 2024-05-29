0c0ef18c06bc670c340b76e9c2976245dc918f3f
nambkvn2004 <nambkvn2004@gmail.com>
2024-05-29 15:48:40 +0700

    npn project

---
+++ NpnLesson06/NpnLesson06/Controllers/NpnSongController.cs
@@ -0,0 +1,42 @@
+﻿
+using NpnLesson06.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NpnLesson06.Controllers
+{
+    public class NpnSongController : Controller
+    {
+        private static List<NpnSong> npnSongs = new List<NpnSong>() 
+        { 
+            new NpnSong{Id=221090047,NpnTitle="Nguyễn Phú Nam",NpnAuthor="K22CNT3",NpnArtist="NTU",NpnYearRelease=2020},
+            new NpnSong{Id=1,NpnTitle="Hoàng Quốc Minh",NpnAuthor="K22CNT3",NpnArtist="NTU",NpnYearRelease=2020}
+        };
+      
+
+        // GET: NpnSong
+        /// <summary>
+        ///    Hiển Thị danh sách bài hát
+        ///    Author: Nguyễn Phú Nam
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult NpnIndex()
+        {
+            return View(npnSongs);
+        }
+        //Get: NpnCreate
+        /// <summary>
+        /// Form thêm mới bài hát
+        /// Author:Nguyễn Phú Nam
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult NpnCreate() 
+        { 
+            var npnSong = new NpnSong();
+            return View(); 
+        }
+    }
+}
---
+++ NpnLesson06/NpnLesson06/Models/NpnSong.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NpnLesson06.Models
+{
+    public class NpnSong
+    {
+        [Key]
+
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Npn: Hãy nhập tiêu đề")]
+        [DisplayName("Tiêu đề")]
+        public string NpnTitle { get; set; }
+        [Required(ErrorMessage ="Npn: Hãy nhập tác giả")]
+        [DisplayName("Tác giả")]
+        public string NpnAuthor { get; set; }
+        [Required(ErrorMessage ="Npn: Hãy nhập nghệ sĩ")]
+        [StringLength(50,MinimumLength =2,ErrorMessage ="Npn: Tên nghệ sĩ có tối thiểu 2 ký tự, tối đa 50 ký tự")]
+        [DisplayName("Nghệ sĩ")]
+        public string NpnArtist { get; set;}
+        [Required(ErrorMessage ="Npn: Hãy nhập năm xuất bản")]
+        [RegularExpression(@"[0-9]{4}",ErrorMessage ="Npn:Năm xuất bản phải có 4 ký tự số")]
+        [Range(1900,2024,ErrorMessage ="Npn: Năm xuất bản trong khoảng 1900 - 2024")]
+        [DisplayName("Năm xuất bản")]
+        public int NpnYearRelease{ get; set;}
+    }
+}
